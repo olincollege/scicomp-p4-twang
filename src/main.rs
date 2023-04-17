c0cb379abe373bb742cf8ca5f9d5267c2b9ebf6d
Aydin O'Leary <aydinoleary@gmail.com>
2023-04-17 11:34:21 -0400

    add functioning (?) delay feedback loop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use fundsp::delay;
+use fundsp::hacker::*;
+
+const SAMPLE_RATE: f64 = 44100.0;
 
 fn main() {
-    let waveguide = delay::Delay()
+    let length = 4.0;
+    let waveguide: An<Delay<f64>> = delay(length);
+
+    let feedback_loop = feedback(waveguide >> lowpass_hz(1000.0, 1.0));
 }
